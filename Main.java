beca9e215d17ef4fe8a4a3e9d57da7ce26c7799b
Serhii Hrynkevych <s.hrynkevych20@gmail.com>
2021-10-01 13:47:21 +0300

    Added overloaded method print to the file Main.java

--- Main.java
+++ Main.java
@@ -8,4 +8,10 @@ public class Main {
     public static void print(String line) {
         System.out.println(line);
     }
+
+    public static void print(String line, int i) {
+        for (int j = 0; j < i; j++) {
+            System.out.println(line);
+        }
+    }
 }
